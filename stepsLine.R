586c7113f8190ceda690ad00be57f8f448c57dbd
Kenneth J Davis <caluchko@users.noreply.github.com>
2014-05-23 22:01:55 -0400

    Update stepsLine.R
    added comments

--- stepsLine.R
+++ stepsLine.R
@@ -5,22 +5,20 @@ stepsLine <- function (filename = "Export.csv", username="caluchko", key="cqdk1g
                        LOWESS = TRUE){
   p <- plotly(username="caluchko", key="cqdk1g1p7k")
   steps <- read.csv(filename, header = TRUE)
-  steps$date1 <- as.Date(steps[,1], format = "%m/%d/%y")
-  steps [,1] <- format(steps$date1, format = "%Y-%m-%d 00:00:00")
+  steps$date1 <- as.Date(steps[,1], format = "%m/%d/%y") # column of date values as date class
+  steps [,1] <- format(steps$date1, format = "%Y-%m-%d 00:00:00") # additional column of date values as character vector format readable to plot.ly API
   
-  steps <- steps[steps[,2] != 0,]
-  steps <- steps[2:length(steps$Steps),]
+  steps <- steps[steps[,2] != 0,] # removes all values of steps that equal zero
+  steps <- steps[2:length(steps$Steps),] # removes most recent day's entry
   
-  Steps <- with (steps, list(
-    x = Date, y = Steps, name = "Steps"
-  ))
+  Steps <- with (steps, list(x = Date, y = Steps, name = "Steps")) # list of x and y values of steps
   
-  fit <- with (steps, lowess(date1, Steps, f = .2)) # note non-default f 
-  Lowess <- list(x = steps$Date, y = rev(fit$y), name = "Lowess")
+  fit <- with (steps, lowess(date1, Steps, f = .2)) # LOWESS fit, note non-default f 
+  Lowess <- list(x = steps$Date, y = rev(fit$y), name = "Lowess") # list of x and y values for fit
   
-  response <- p$plotly(Steps, Lowess, kwargs=list(filename="test2", fileopt="overwrite"))
+  response <- p$plotly(Steps, Lowess, kwargs=list(filename="test2", fileopt="overwrite")) #plot.ly API function
   
-  url <- response$url
+  url <- response$url # URL of new plot on plot.ly
   filename <- response$filename
   
   plot(steps$date1, steps$Steps, type = "l")
